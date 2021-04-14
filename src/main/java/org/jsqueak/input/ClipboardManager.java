4067b05d173daa8d6ec880288640a65160e4d248
ricky9090 <richie_xie@126.com>
2021-04-14 11:38:09 +0800

    Improve the clipboard function

--- src/main/java/org/jsqueak/input/ClipboardManager.java
+++ src/main/java/org/jsqueak/input/ClipboardManager.java
@@ -2,6 +2,12 @@ package org.jsqueak.input;
 
 import org.jsqueak.core.SqueakObject;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+
 public class ClipboardManager {
 
     String clipboard;
@@ -19,10 +25,15 @@ public class ClipboardManager {
 
     public void clipboardWrite(String str) {
         this.clipboard = str;
+
+        writeToSystemClipboard(str);
     }
 
     public void clipboardWrite(SqueakObject aStringObj) {
-        clipboard = aStringObj.asString();
+        String asStr = aStringObj.asString();
+        clipboard = asStr;
+
+        writeToSystemClipboard(asStr);
     }
 
     public String getClipboard() {
@@ -32,10 +43,43 @@ public class ClipboardManager {
     public SqueakObject clipboardRead() {
         String target;
         if (clipboard == null) {
-            target = "";
+            target = readSystemClipboard();
         } else {
-            target = clipboard;
+            // Because we write system clipboard every time the clipboardWrite() called
+            // so if there is a difference between two clipboards,
+            // the system one should have been updated
+            String systemClipboardText = readSystemClipboard();
+            if (clipboard.equals(systemClipboardText)) {
+                target = clipboard;
+            } else {
+                target = systemClipboardText;
+            }
         }
         return SqueakObject.createSTString(target);
+    }
+
+    private void writeToSystemClipboard(String str) {
+        Clipboard systemClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        Transferable transText = new StringSelection(str);
+        systemClipboard.setContents(transText, null);
+    }
+
+    private String readSystemClipboard() {
+        String result = "";
+
+        Clipboard systemClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        Transferable systemClipContent = systemClipboard.getContents(null);
+
+        if (systemClipContent != null) {
+            if (systemClipContent.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                try {
+                    result = (String) systemClipContent.getTransferData(DataFlavor.stringFlavor);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return result;
     }
 }
